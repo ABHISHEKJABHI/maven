573d0026bcde3a2d801b8a359c96fd624393b543
ABHISHEKJABHI <ashek7944@gmail.com>
2025-05-28 14:16:54 +0000

    1st javafile

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+
+/**
+ * i am going to be pirate king
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("i am going to be pirate king");
+    }
+}
